309be357731894969b976de901c1dc4776e25194
smalls <stsmall@gmail.com>
2019-12-12 13:57:35 -0500

    added space and locus count out

--- gff2fastaAln.py
+++ gff2fastaAln.py
@@ -163,14 +163,14 @@ def write_outfile(s_ix: int,
     None
 
     """
-    with open(f"{fname}.{chrom}.{s_ix}-{e_ix}.txt", 'w') as out_file:
+    with open(f"{fname}.{chrom}.{s_ix}-{e_ix}.{len(loci_list}.txt", 'w') as out_file:
         for item in zip(header_list, loci_list):
             headers, seqs = item
             if bpp is True:
                 out_file.write(f"{len(headers)} {len(seqs[0])}\n\n")
             for sample, dna in zip(headers, seqs):
                 if bpp is True:
-                    out_file.write(f"^{sample}{' '*(just-len(sample))}{dna}\n")
+                    out_file.write(f"^{sample}{' '*(just-len(sample))}{dna}\n\n")
                 else:
                     out_file.write(f">{sample}\n{dna}\n")
     return(None)
