800dcd17b0e33ccb9b7e688d11af8f4e70cf370b
smalls <stsmall@gmail.com>
2020-01-15 20:09:45 -0500

    d

--- parse_twisst_dist_len.py
+++ parse_twisst_dist_len.py
@@ -243,7 +243,6 @@ def calc_mrca(chrm, topos, blen_data, min_freq, tree_count):
             topo = f"topo{i + 1}"
             nancount = sum(np.isnan(mrca))
             freq = (1 - (nancount/tree_count))
-            breakpoint()
             if freq >= min_freq:
                 topos_freq.append(f"{topo}")  # pass min_freq
                 if topo in topos:
