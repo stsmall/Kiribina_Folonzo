241293144c57f93336893136937d1b4e5b54f320
smalls <stsmall@gmail.com>
2020-02-10 17:25:26 -0500

    MWU test

--- T1T2Stats.py
+++ T1T2Stats.py
@@ -4,6 +4,7 @@
 import sys
 from Bio import Phylo as phy
 import numpy as np
+from scipy import stats
 
 
 def distanceAB_T1T2(tree, AB_T1, AB_T2):
@@ -46,13 +47,19 @@ def t1t2_stats_out(AB_T1, AB_T2, BC_T1, BC_T2, AC_T1, AC_T2):
     sp23T2 = np.mean(AB_T2)
     sp34T2 = np.mean(BC_T2)
     sp24T2 = np.mean(AC_T2)
-    # KS test
+    # MWU test
+    xT2u, xT2p = stats.mannwhitneyu(sp23T2, sp34T2, alternative='greater')
+    yT2u, yT2p = stats.mannwhitneyu(sp23T2, sp24T2, alternative='greater')
+    zT2u, zT2p = stats.mannwhitneyu(sp34T2, sp24T2, alternative='greater')
 
     # T1
     sp234T1 = np.mean(AB_T1)
     sp342T1 = np.mean(BC_T1)
     sp423T1 = np.mean(AC_T1)
-    # KS test
+    # MWU test
+    xT1u, xT1p = stats.mannwhitneyu(sp234T1, sp342T1, alternative='greater')
+    yT1u, yT1p = stats.mannwhitneyu(sp234T1, sp423T1, alternative='greater')
+    zT1u, zT1p = stats.mannwhitneyu(sp342T1, sp423T1, alternative='greater')
 
     # internal node length
     sp2C2 = sp234T1 - sp23T2
@@ -62,6 +69,14 @@ def t1t2_stats_out(AB_T1, AB_T2, BC_T1, BC_T2, AC_T1, AC_T2):
     print(f"({sp2},{sp3}),{sp4}  T2:{sp23T2} T1:{sp234T1} C2:{sp2C2}")
     print(f"({sp3},{sp4}),{sp2}  T2:{sp34T2} T1:{sp342T1} C2:{sp3C2}")
     print(f"({sp4},{sp2}),{sp3}  T2:{sp24T2} T1:{sp423T1} C2:{sp4C2}")
+
+    print(f"{sp2},{sp3} gt {sp3},{sp4} = pvalue {xT2p}")
+    print(f"{sp2},{sp3} gt {sp2},{sp4} = pvalue {yT2p}")
+    print(f"{sp3},{sp4} gt {sp2},{sp4} = pvalue {zT2p}")
+
+    print(f"({sp2},{sp3}),{sp4} gt ({sp3},{sp4}),{sp2} = pvalue {xT1p}")
+    print(f"({sp2},{sp3}),{sp4} gt ({sp2},{sp4}),{sp3} = pvalue {yT1p}")
+    print(f"({sp3},{sp4}),{sp2} gt ({sp2},{sp4}),{sp3} = pvalue {zT1p}")
 
 
 if __name__ == "__main__":
