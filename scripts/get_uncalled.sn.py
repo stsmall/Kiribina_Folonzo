3fc959423f02755f501a2e14cba322c4659397f8
smalls <stsmall@gmail.com>
2020-09-14 11:03:39 -0400

    d

--- scripts/get_uncalled.sn.py
+++ scripts/get_uncalled.sn.py
@@ -5,6 +5,7 @@ import gzip
 uncalled_dict = {}
 with gzip.open(snakemake.input[0]) as vcf:
     for line in vcf:
+        line = line.decode()
         if line.startswith("#CHROM"):
             sample_line = line.split()[9:]
             for sample in sample_line:
@@ -19,6 +20,6 @@ with gzip.open(snakemake.input[0]) as vcf:
                 if "./." in gt.split(":")[0]:
                     uncalled_dict[sample].append("pos")
 
-with open(snakemake.output[1], 'w') as uncalled:
+with open(snakemake.output[0], 'w') as uncalled:
     for sample in sample_line:
         uncalled.write(f'{sample}/t{uncalled_dict[sample].join("/t")}/n')
